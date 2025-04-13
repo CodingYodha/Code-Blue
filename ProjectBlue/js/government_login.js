bba091367b778b561a7c40d3b99bc2dac2b05797
Soham Sanjay Pawar <sohampawar1866@gmail.com>
2025-04-13 21:53:49 +0530

    Delete ProjectBlue directory

--- ProjectBlue/js/government_login.js
+++
@@ -1,8 +0,0 @@
-/* government_login.js */
-document.getElementById('login-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    // Here, you would typically make an API call to authenticate the user.
-    // For now, let's just simulate a successful login.
-    alert('Login successful!');
-    window.location.href = 'dashboard/dashboard.html'; // Redirect to dashboard
-  });
